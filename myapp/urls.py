e4b47dfd57690a36737a495b189213a9df81c929
Farhad0o0 <mfarhadt117@gmail.com>
2025-12-28 20:29:37 +0330

    fix some bugs
    fix login and sing up  forms and link them

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("",views.index,name="index"),
+    path("sign_up",views.signup_page,name="signup"),
+    path("dashboard",views.dashboard_page,name="dashboard_"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+
+def signup_page(request):
+    return render(request,"sign_up.html")
+
+def dashboard_page(request):
+    return render(request,"dashboard.html")

eddd381cd3d1ab036bfe382baf66ae1fab3e83f1
Farhad0o0 <mfarhadt117@gmail.com>
2026-01-01 03:39:41 +0330

    making things better

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path , include
 from . import views
 
 urlpatterns = [
-    path("index",views.index_page,name="index_"),
+    path("",views.index_page,name="index_"),
     path("sign_up",views.signup_page,name="signup"),
     path("dashboard",views.dashboard_page,name="dashboard_"),
     path("my_room",views.my_room_page,name="my_room_"),
